6c7a70f0d9f8ebc5b2c2362c96d60eb11c70eefc
utsavpatoliya <utsavpatoliya3110@gmail.com>
2025-07-02 23:40:32 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import FoodDonate from './components/FoodDonate';
 import './App.css';
 
 // Set default axios base URL
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = 'https://greenbite-lbw4.onrender.com/';
 
 function App() {
   const [user, setUser] = useState(null);
